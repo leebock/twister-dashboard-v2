ad696cd08baded920394f5b14edffae74411dd06
leebock <lbock@esri.com>
2021-07-13 16:59:22 -0400

    fetching twisters and adding to map.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ const FEATURE_SERVICE_URL = "http://services.arcgis.com/nzS0F0zdNLvs7nc8/ArcGIS/
 function App() {
     
     const [summary, setSummary] = useState([]);
-    const [selected, setSelected] = useState(null);    
+    const [selected, setSelected] = useState(null);  
+    const [twisters, setTwisters] = useState([]);
 
     useEffect(
         () => {
@@ -21,6 +22,20 @@ function App() {
             );            
         },
         []
+    );
+
+    useEffect(
+        () => {
+            if (selected) {
+                fetchTwisters(
+                    selected.Year,
+                    (result)=>{
+                        setTwisters(result.features);
+                    }
+                )            
+            }
+        },
+        [selected]
     );
 
     function countByYear(callBack)
@@ -37,6 +52,15 @@ function App() {
 		query.groupByFieldsForStatistics = ["Year"];
         executeQueryJSON(FEATURE_SERVICE_URL, query).then(callBack);        	
 
+    }
+    
+    function fetchTwisters(year, callBack)
+    {
+        var query = new Query();
+		query.where = "F_Scale > -9 and Year = "+year;
+        query.outFields = ["*"];
+        query.returnGeometry = true;
+        executeQueryJSON(FEATURE_SERVICE_URL, query).then(callBack);        	
     }
     
     const selectRecord = (year) => {
@@ -70,11 +94,14 @@ function App() {
               
               <div className="col d-flex flex-column position-relative overflow-hidden bg-info">
                 <h3 className="h4">Numbers</h3>
+                {selected && <div>Year: <strong>{selected.Year}</strong></div>}
+                {selected && <div>Count: <strong>{selected.totalCount}</strong></div>}
+                {selected && <div>Check: <strong>{twisters.length}</strong></div>}
               </div>
               
               <div className="col d-flex flex-column position-relative overflow-hidden bg-success">
                 <h3 className="h4">Map</h3>
-                <TMap className="flex-grow-1"/>
+                <TMap className="flex-grow-1" twisters={twisters}/>
               </div>
               
           </div>
--- src/components/TMap.js
+++ src/components/TMap.js
@@ -2,12 +2,17 @@ import React, { useEffect } from 'react';
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import Extent from "@arcgis/core/geometry/Extent";
+import Point from "@arcgis/core/geometry/Point";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import Graphic from "@arcgis/core/Graphic";
 
-export const TMap = ({className}) => {
+export const TMap = ({className, twisters}) => {
+    const _layerTwisters = React.useRef(new GraphicsLayer());
     useEffect(
         () => {
             console.log("map::creating map");
             const map = new Map({ basemap: "gray-vector"});
+            map.add(_layerTwisters.current);
             new MapView(
                 {
                     map: map, 
@@ -19,5 +24,19 @@ export const TMap = ({className}) => {
         },
         []
     );        
+    useEffect(
+        () => {
+            console.log("map::populating layer");
+            _layerTwisters.current.removeAll();
+            twisters.forEach((item, i) => {
+                _layerTwisters.current.add(
+                    new Graphic({geometry: new Point(item.geometry.x, item.geometry.y)})
+                );
+            });
+            return () => {};
+        },
+        [twisters]
+    );        
+
     return (<div id="map" className={className}></div>);
 }
