e22bb9ede73e4ab9cd87627ee6a963bf580c9526
leebock <lbock@esri.com>
2021-08-17 18:06:40 -0400

    only write summary contents once there's an actual summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,20 +2,27 @@ export const Summary = ({className, summary, twisters}) => {
     const year = twisters.length ? twisters[0].Year : "";
     return (
         <div id="summary" className={className}>
-            <h3>{year}</h3>
-            <span>Year</span>
-            <h3>{summary && summary.totalCount.toLocaleString()}</h3>
-            <hr/>
-            <span>Total tornadoes</span>
-            <h3>{summary && summary.totalInjuries.toLocaleString()}</h3>
-            <hr/>
-            <span>Injuries</span>
-            <h3>{summary && summary.totalFatalities.toLocaleString()}</h3>
-            <hr/>
-            <span>Fatalities</span>
-            <h3>{summary && summary.totalLoss.toLocaleString()}</h3>
-            <hr/>
-            <span>Property Loss ($millions)</span>
+            {
+                summary &&
+                (
+                    <>
+                    <h3>{year}</h3>
+                    <span>Year</span>
+                    <h3>{summary && summary.totalCount.toLocaleString()}</h3>
+                    <hr/>
+                    <span>Total tornadoes</span>
+                    <h3>{summary && summary.totalInjuries.toLocaleString()}</h3>
+                    <hr/>
+                    <span>Injuries</span>
+                    <h3>{summary && summary.totalFatalities.toLocaleString()}</h3>
+                    <hr/>
+                    <span>Fatalities</span>
+                    <h3>{summary && summary.totalLoss.toLocaleString()}</h3>
+                    <hr/>
+                    <span>Property Loss ($millions)</span>        
+                    </>
+                )
+            }
         </div>
     );
 }
