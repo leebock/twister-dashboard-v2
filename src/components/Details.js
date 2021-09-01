a35edc72447599a4a123e2d924c7902db1bef354
leebock <lbock@esri.com>
2021-09-01 15:06:52 -0400

    added <hr>'s for details

--- src/components/Details.js
+++ src/components/Details.js
@@ -14,22 +14,27 @@ export const Details = ({className, twister}) => {
                     </li>
                     <li>
                         <h3 className="emphasized">{twister.F_Scale}</h3>
+                        <hr/>
                         <span className="caption">Enhanced Fujita Scale</span>
                     </li>
                     <li>
                         <h3>{Math.round(twister.Length_mi*10)/10}</h3>
+                        <hr/>
                         <span className="caption">Length (miles)</span>
                     </li>
                     <li>
                         <h3>{twister.Fatalities}</h3>
+                        <hr/>
                         <span className="caption">Fatalities</span>
                     </li>
                     <li>
                         <h3>{twister.Injuries}</h3>
+                        <hr/>
                         <span className="caption">Injuries</span>
                     </li>
                     <li>
                         <h3>{Math.round(twister.Loss*100)/100}</h3>
+                        <hr/>
                         <span className="caption">Property Loss ($millions)</span>
                     </li>
                 </ul>
